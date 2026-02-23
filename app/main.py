f5a7f4e975afacc543ac0856f42a256d5983cc04
Juan Pablo Sarmiento <juan.sartru@gmail.com>
2026-02-22 19:32:33 -0800

    Added README.md, index.html to testing and vercel deployment

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import FileResponse
 
 from app.routers.resources import router as resources_router
 from app.routers.events import router as events_router
@@ -11,6 +12,10 @@ app = FastAPI(title="Supabase Logistics API (FastAPI)")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/test")
+def test():
+    return FileResponse("index.html")
 
 app.include_router(resources_router)
 app.include_router(events_router)
